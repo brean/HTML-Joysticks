3fd2848e1e63e6e76f80eb8b8ef40531cabd6462
Andreas Bresser <self@andreasbresser.de>
2021-03-18 00:17:36 +0100

    fix multi-touch using pointerId

--- js/joystick_controller.js
+++ js/joystick_controller.js
@@ -14,7 +14,7 @@ class JoystickController {
 		this.dragStart = null;
 
 		// track touch identifier in case multiple joysticks present
-		this.touchId = null;
+		this.pointerId = -1;
 		
 		this.value = { x: 0, y: 0 }; 
 
@@ -26,48 +26,24 @@ class JoystickController {
 	}
 
 	handleDown(event) {
+		event.preventDefault();
+		this.pointerId = event.pointerId;
 		document.addEventListener('pointermove', this._handleMove, {passive: false});
 		document.addEventListener('pointerup', this._handleUp);
 
 		// all drag movements are instantaneous
 		this.stick.style.transition = '0s';
 
-		// touch event fired before mouse event; prevent redundant mouse event from firing
-		event.preventDefault();
-
-		if (event.changedTouches) {
-			this.dragStart = { x: event.changedTouches[0].clientX, y: event.changedTouches[0].clientY };
-		} else {
-			this.dragStart = { x: event.clientX, y: event.clientY };
-		}
-
-	// if this is a touch event, keep track of which one
-		if (event.changedTouches) {
-			self.touchId = event.changedTouches[0].identifier;
-		}
+		event.target.id = this.thumbId;
+		this.dragStart = { x: event.clientX, y: event.clientY };
 	}
 		
 	handleMove(event) {
-    event.preventDefault();
-		// if this is a touch event, make sure it is the right one
-		// also handle multiple simultaneous touchmove events
-		let touchmoveId = null;
-		if (event.changedTouches)
-		{
-			for (let i = 0; i < event.changedTouches.length; i++)
-			{
-				if (this.touchId == event.changedTouches[i].identifier)
-				{
-					touchmoveId = i;
-					event.clientX = event.changedTouches[i].clientX;
-					event.clientY = event.changedTouches[i].clientY;
-				}
-			}
-
-			if (touchmoveId == null) {
-        return;
-      }
+		if (this.pointerId !== event.pointerId) {
+			return;
 		}
+    event.preventDefault();
+		
 		const maxDistance = this.maxDistance;
 		const deadzone = this.deadzone;
 
@@ -96,17 +72,15 @@ class JoystickController {
 	}
 
 	handleUp(event) {
-		// if this is a touch event, make sure it is the right one
-		if (event.changedTouches && this.touchId != event.changedTouches[0].identifier) {
-      return;
-    }
+		if (this.pointerId !== event.pointerId) {
+			return;
+		}
 		// transition the joystick position back to center
 		this.stick.style.transition = '.2s';
 		this.stick.style.transform = `translate3d(0px, 0px, 0px)`;
 
 		// reset everything
 		this.value = { x: 0, y: 0 };
-		this.touchId = null;
 		document.removeEventListener('pointermove', this._handleMove, {passive: false});
 		document.removeEventListener('pointerup', this._handleUp);
 	}
